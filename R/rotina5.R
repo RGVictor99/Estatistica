83615ae207228647602f82509fd21f508d8efb23
Victor Ribeiro <87677553+RGVictor99@users.noreply.github.com>
2022-10-17 15:29:00 -0300

    Add files via upload

---
+++ R/rotina5.R
@@ -0,0 +1,11 @@
+DadosN=c (6,5,8,5,6,7,6,6)
+dados1 <- DadosN
+
+DadosT=c (8,6,5,7,4,9,5,6)
+dados2 <- DadosT
+
+mediaN=mean(DadosN)
+mediaT=mean(DadosT)
+
+cvN = sd(DadosN)*100/mediaN;cvN
+cvT=sd(DadosT)*100/mediaT;cvT
